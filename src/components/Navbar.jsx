300a98c6df08547aa48ddc8ccbeffb7a0cdb76be
Thiago <thiagogoisjunior@gmail.com>
2023-07-11 18:08:17 -0300

    ✔ Resolvendo commit na Vercel com o "Link"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { Github, Plus } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar(){
     return(
         <div className="bg-zinc-800 h-24 w-full flex items-center justify-between p-5">
-        <a href="/" className="font-bold text-xl">Nord</a>
-        <a
+        <Link href="/" className="font-bold text-xl">Nord</Link>
+        <Link
           className="hover:text-zinc-300"
           href="https://github.com/iamgois/iamgois-blog"
         >
           <Github />
-        </a>
+        </Link>
       </div>
     )
 }

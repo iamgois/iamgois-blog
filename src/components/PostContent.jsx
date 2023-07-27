ceade738db9516acb09f5599e23ced70ebc52ba6
Thiago <thiagogoisjunior@gmail.com>
2023-07-26 23:44:51 -0300

    ✔ Consertando vizualização para mobile

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -36,26 +36,25 @@ const PostList = () => {
     </div>
   } else{
     return (
-      <div>
-        <Link className="fixed bottom-5 right-5 bg-zinc-800 p-2 rounded-full transition hover:bg-zinc-700 [&>*:nth-child(2)]:hover:block flex" href="/login">
+      <div className="pb-10">
+        <Link className="fixed bottom-5 right-5 bg-zinc-800 p-3 rounded-full transition hover:bg-zinc-700" href="/login">
           <Plus />
-          <p className="hidden px-2">Postar</p>
         </Link>
         {/* <h1>Lista de Posts</h1> */}
         {items.map((post) => (
           <a href={`/posts/${post._id}`} className="hover:text-zinc-300" key={post.id}>
-            <h2 className="mt-3">
+            <h2 className="mt-4 font-semibold">
               {post.title.length > 80
                 ? `${post.title.slice(0, 80)}...`
                 : post.title}
             </h2>
-            <div className="flex justify-between items-end font-thin text-sm">
+            <div className="flex justify-between items-end font-normal text-sm text-zinc-500">
               <p>
                 {post.subtitle.length > 55
                   ? `${post.subtitle.slice(0, 55)}...`
                   : post.subtitle}
               </p>
-              <p className="text-xs text-zinc-400">{post.createdAt}</p>
+              <p className="text-xs text-zinc-600">{post.createdAt}</p>
             </div>
           </a>
         ))}
